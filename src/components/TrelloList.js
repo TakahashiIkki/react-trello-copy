1a6522fbe650f9973ad47d230a629776b28c7fd6
Takashi Ikki <takahashi@omicale.com>
2020-06-06 21:45:15 +0900

    ドラッグ&ドロップを対応した

--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,16 +1,33 @@
 import React from "react";
 import TrelloCard from "./TrelloCard";
 import TrelloActionButton from "./TrelloActionButton";
+import {Droppable} from "react-beautiful-dnd";
 
 const TrelloList = ({title, cards, listID}) => {
+    console.log(cards);
     return (
-        <div style={style.container}>
-            <h4>{title}</h4>
-            {cards.map(card =>
-                <TrelloCard key={card.id} text={card.text}/>
+        <Droppable droppableId={String(listID)}>
+            {provided => (
+                <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={style.container}
+                >
+                    <h4>{title}</h4>
+                    {cards.map((card, index) => (
+                        <TrelloCard
+                            key={card.id}
+                            text={card.text}
+                            index={index}
+                            id={card.id}
+                            listID={listID}
+                        />
+                    ))}
+                    <TrelloActionButton listID={listID}/>
+                    {provided.placeholder}
+                </div>
             )}
-            <TrelloActionButton listID={listID}/>
-        </div>
+        </Droppable>
     )
 }
 
